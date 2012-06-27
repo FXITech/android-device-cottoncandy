1db574fd1ec95c6fa4f03b1c76796df2f8c5ef55
Haavard Kvaalen <havardk@fxitech.com>
2012-06-27 16:31:18 +0200

    Change the wifi related setup to match the cotton candy.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,11 +24,23 @@ BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := false
 BOARD_HAVE_BLUETOOTH_CSR := true
 
-WPA_SUPPLICANT_VERSION := VER_0_8_X
-BOARD_WPA_SUPPLICANT_DRIVER := WEXT
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := private_lib_driver_cmd
-WIFI_DRIVER_MODULE_PATH := "/system/modules/ath6kl.ko"
-WIFI_DRIVER_MODULE_NAME := "ath6kl"
+# Wifi related defines
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+WPA_SUPPLICANT_VERSION      := VER_0_8_X
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER        := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_bcmdhd
+BOARD_WLAN_DEVICE           := bcmdhd
+WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA     := "/vendor/firmware/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_P2P     := "/vendor/firmware/fw_bcmdhd_p2p.bin"
+WIFI_DRIVER_FW_PATH_AP      := "/vendor/firmware/fw_bcmdhd_apsta.bin"
+
+# TODO(havardk): This causes libhardware_legacy to load the kernel module.
+# Tuna does not use this, so should be unnecessary.  Figure it out.
+WIFI_DRIVER_MODULE_PATH := "/system/modules/bcmdhd.ko"
+WIFI_DRIVER_MODULE_NAME := "bcmdhd"
+
 
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
