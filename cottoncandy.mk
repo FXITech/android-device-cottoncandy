75c47549abdd2e318898359d997f51f231b1d3af
Thomas Langaas <thomas.langas@fxitech.com>
2012-07-11 15:45:43 +0200

    Changes various product strings to something that compiles

--- cottoncandy.mk
+++ cottoncandy.mk
@@ -3,8 +3,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, device/fxi/cottoncandy/common.mk)
 $(call inherit-product, device/fxi/cottoncandy/device.mk)
 
-PRODUCT_BRAND := "FXI"
-PRODUCT_DEVICE := "Cotton Candy"
-PRODUCT_NAME := "Cotton Candy"
-PRODUCT_MODEL := "Cotton Candy"
-PRODUCT_MANUFACTURER := "FXI"
+PRODUCT_BRAND := FXI
+PRODUCT_DEVICE := cottoncandy
+PRODUCT_NAME := cottoncandy
+PRODUCT_MODEL := Cotton Candy
+PRODUCT_MANUFACTURER := FXI
