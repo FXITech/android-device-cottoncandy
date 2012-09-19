7fedf03b144804e0b6bdc40098bcc500e1da0ec2
Haavard Kvaalen <havardk@fxitech.com>
2012-09-19 10:44:21 +0200

    Copy prebuilt uboot binaries into the output directory.
    We previously used a symbolic link that made assumptionsn about
    the position of the output directory which was not always true.
    These files are small anyway.
    
    Change-Id: I749332620556eac581789f237812e13902bbb358

--- uboot.mk
+++ uboot.mk
@@ -1,12 +1,12 @@
 
 $(PRODUCT_OUT)/u-boot.bin:
-	ln -sf ../../../../device/fxi/cottoncandy/u-boot.bin $(PRODUCT_OUT)/u-boot.bin
+	$(hide) cp $(TOP)/device/fxi/cottoncandy/u-boot.bin $(PRODUCT_OUT)/u-boot.bin
 
 $(PRODUCT_OUT)/u-boot-bl1.bin:
-	ln -sf ../../../../device/fxi/cottoncandy/u-boot-bl1.bin $(PRODUCT_OUT)/u-boot-bl1.bin
+	$(hide) cp $(TOP)/device/fxi/cottoncandy/u-boot-bl1.bin $(PRODUCT_OUT)/u-boot-bl1.bin
 
 .PHONY: u-boot-bl1
 u-boot-bl1: $(PRODUCT_OUT)/u-boot-bl1.bin
-	cp $(PRODUCT_OUT)/u-boot-bl1.bin $(PRODUCT_OUT)/boot
+	$(hide) cp $(PRODUCT_OUT)/u-boot-bl1.bin $(PRODUCT_OUT)/boot
 
 BOOTLOADER_TARGETS += u-boot-bl1
